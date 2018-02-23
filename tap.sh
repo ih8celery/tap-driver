7cf17aae1a51d4fcdb1f9bed932144b489d791f6
Adam Marshall <adamm3992@gmail.com>
2018-02-23 15:29:38 -0500

    rename perl script and add simpler sh script

---
+++ tap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# simple test driver that runs a list of executables in order
+#
+
+while [ ! "$#" -eq 0 ]; do
+  if [ -x "$1" ]; then
+   dir=`cd $(dirname $1) && pwd`
+   file=$(basename $1)   
+   $dir/$file 
+  else
+    echo "error: expected argument to be executable"
+    exit 1
+  fi
+
+  shift
+done
